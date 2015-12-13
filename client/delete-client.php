65b31b36f1c677ec65216374533e8db8a41399ea
AdiNurlemma <nurlemma.adi@gmail.com>
2015-12-13 16:54:10 +0700

    Perubahaan Kesembilan

--- client/delete-client.php
+++ client/delete-client.php
@@ -1,9 +1,8 @@
 <?php
 	require_once('../nusoap/lib/nusoap.php');
-	$client= new nusoap_client('http://tigaab.burhanudin.me/server.php?wsdl',true);
+	$client= new nusoap_client('http://localhost/ta/server.php?wsdl',true);
 	$result = $client->call('deleteBpjs',array(
 			'no_bpjs' => $_POST['no_bpjs'],
-			'no_kk'=> $_POST['no_kk'], 
 			'no_pegawai' =>$_POST['no_pegawai']
 		));
 ?>
